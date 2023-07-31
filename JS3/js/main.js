9c01e03cb5f9ec3db6757b54f96d98d6b6e4de5b
mcwong644 <mcwong644@gmail.com>
2023-07-31 17:42:03 +0800

    first commit

---
+++ JS3/js/main.js
@@ -0,0 +1,17 @@
+//Strings
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#static_methods
+// https://youtu.be/EfAl9bwzVZk
+// JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
+// Dave Grey chapter 2 stop 22:13 chapter 3
+
+
+const myVariable = "Mathematics";
+
+console.log("Length of string is ",myVariable.length);
+console.log(`String is ${myVariable}`);
+console.log(`char at 5 is "${myVariable.charAt(5)}"`);
+console.log(`index of "ati" is ${myVariable.indexOf("ati")}`);
+console.log(myVariable.slice(5,8));
+console.log(myVariable.slice(5));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.split('e'));
